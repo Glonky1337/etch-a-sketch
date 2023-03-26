6d053fb98e7e68950a413939219c0673234ed963
Glonky1337 <fergusontbs@gmail.com>
2023-03-26 13:54:45 -0400

    Add functionality to color buttons in JavaScript

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Set an initial global color value
+let globalColor = 'overlay';
 const gridContainer = document.getElementById("grid-container");
 
 function generateGrid(rows, columns) {
@@ -6,8 +8,30 @@ function generateGrid(rows, columns) {
     for (i = 0; i < (rows * columns); i++) {
         let cell = document.createElement("div");
         gridContainer.appendChild(cell).className = "grid-item";
+        cell.addEventListener('mouseover', function(e) {
+            console.log(globalColor);
+            if (globalColor === 'black') {
+                cell.style.backgroundColor = 'rgb(1,1,1)';
+                cell.style.opacity = '1';
+            } else if (globalColor === 'overlay') {
+                let opacity = cell.style.opacity;
+                if (cell.classList.contains('grid-item')) {
+                    cell.style.backgroundColor = 'rgb(1,1,1)';
+                    cell.style.opacity = (Number(opacity) + 0.2);
+                } 
+            } else if (globalColor === 'candy') {
+                    cell.style.backgroundColor = 'rgb(255,159,196)';
+                    cell.style.opacity = '1';
+            } else {
+                    globalColor = 'black';
+            }
+        })
     };
 };
+
+document.querySelector('#classic-button').addEventListener('click', () => globalColor = 'black');
+document.querySelector('#overlay-button').addEventListener('click', () => globalColor = 'overlay');
+document.querySelector('#candy-button').addEventListener('click', () => globalColor = 'candy');
 
 function clearGrid() {
     while (gridContainer.firstChild) {
@@ -34,9 +58,4 @@ const largeButton = document.querySelector('#large-button');
 largeButton.addEventListener('click', () => {
     clearGrid();
     generateGrid(40, 60);
-})
-
-
-// #classic-button
-// #overlay-button
-// #candy-button
+});
